a3de0eeb1e8952468e4905efe4a6a73fcb4e02bc
a19xinhu <63045875+a19xinhu@users.noreply.github.com>
2023-04-16 19:05:55 +0200

    Initial Commit

---
+++ Native-IOS/Native_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Native_IOSApp.swift
+//  Native-IOS
+//
+//  Created by Jie Lu on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct Native_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
